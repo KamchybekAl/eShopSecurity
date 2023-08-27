793640c24d9fd2c58fe2eaf111ab6d7cae58e3c6
Alzhanbaev Kamchybek <aljanbaevkam@gmail.com>
2023-08-28 03:41:20 +0600

    Added JwtService

---
+++ eShopSecurity/src/main/java/kg/neo/eShopSecurity/config/ApplicationConfig.java
@@ -0,0 +1,13 @@
+package kg.neo.eShopSecurity.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.UserDetailsService;
+
+@Configuration
+public class ApplicationConfig {
+    @Bean
+    public UserDetailsService userDetailsService(){
+        return
+    }
+}
